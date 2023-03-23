8cfbe980ab7a2d54b4af538fbb24ceaf9bc89a9d
Filipe Alves <filipealvesrds2019@gmail.com>
2023-03-23 19:15:13 -0300

    atualizar

--- Dnc/tech2 day 3/tela_funcao.js
+++ Dnc/tech2 day 3/tela_funcao.js
@@ -1,8 +1,34 @@
- function maioridade(){
-    window.location.href = "tela_escolha.html"
+document.getElementById('nao').addEventListener("click",
+      
 
-}
 
-function menoridade(){
-    window.location.href = "tela_menoridade.html"
-}
+
+    function redirectFunction(){
+        window.location.href = "index.html";
+        window.location.replace("tela_menoridade.html");
+
+
+
+    }
+
+
+
+
+
+)
+
+
+
+
+
+
+
+
+//function maioridade(){
+  //  window.location.href = "tela_escolha.html"
+
+//}
+
+//function menoridade(){
+    //window.location.href = "tela_menoridade.html"
+//}
