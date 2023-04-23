6a025de1a6e30eb63bb7f6c466bfe5ef7139289c
Filipe Alves <filipealvesrds2019@gmail.com>
2023-04-23 00:46:43 -0300

    commit

--- Dnc/CCXP2022/index.js
+++ Dnc/CCXP2022/index.js
@@ -8,14 +8,21 @@ const lancamento = "31 dec 2023"
 
 function countDown(){
     const dataLanc = new Date(lancamento);
-    const hoje = new Data();
+    const hoje = new Date()
 
     const segTotal = (dataLanc - hoje) / 1000;
 
-    const dias = Math.floor(segTotal / 3600 / 24);
+    const dias = Math.floor((segTotal / 3600 / 24));
     const horas = Math.floor((segTotal / 3600) % 24);
-    const minutos = Math.floor((segTotal / 60) % 60);
+    const minutos = Math.floor(segTotal / 60) % 60;
     const segundos = Math.floor(segTotal % 60);
+
+    dia.innerHTML = `<strong>${dias}D</strong>`
+    hora.innerHTML = `<strong>${horas}M</strong>`
+    minuto.innerHTML = `<strong> ${minutos}H</strong>`
+    segundo.innerHTML = `<strong>${segundos}S</strong>`
 
 
 }
+countDown();
+setInterval(countDown, 1000);
